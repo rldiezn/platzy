5efc10e892311f4665584341ce86fc918112747d
Ricardo Diez <rldiezn@gmail.com>
2018-11-14 22:42:56 -0600

    Curso de Angular 4 1. Inicio del curso 2. Introducción a Angular 4 3. Setup del Ambiente de Trabajo   -instalar angular con npm y angular CLI 4. Conceptos Básicos   -manejando conceptos de :     --Data binding & String interpolation     --Property binding     --Event Binding     --Two way data Binding     --Directivas     --Intalando librería Angular Google Maps (agm) 5. Directivas   -ngStyle & ngClass   -ngSwicht   -Directiva de atributo   -Host Listener   -Host Binder 6. Angular UI   -Angular material & Bootstrap   -Configuracion de bootstrap en angular 4

--- cursos/angular4/platziSquare/src/app/app.component.ts
+++ cursos/angular4/platziSquare/src/app/app.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'platziSquare';
+
+  title = 'Platzi square';
+  lenguage = 'es';
+  translate = {
+    lenguage:'es',
+    es:{
+      welcomeMsg:'Bienvenido a '+ this.title,
+      segestion:'Te listamos algunos enlaces que te ayudaran a empezar con Angular 4: ',
+      tourOfHeroes : 'El viaje del Heroe',
+      CLIDocumentation : 'Documentaciòn CLI',
+      angularBlog : 'Blog de Angular 4'
+    },
+    en:{
+      welcomeMsg:'Welcome to '+ this.title,
+      segestion:'Here are some link to help you start:',
+      tourOfHeroes : 'Tour of heroes',
+      CLIDocumentation : 'CLI Documentation',
+      angularBlog : 'Angular blogs'
+    }
+  };
+
+  tourOfHeroes = 'El viaje del Heroe';
+  CLIDocumentation = 'Documentaciòn CLI';
+  angularBlog = 'Blog de Angular 4'
+  //test Math
+  a = 5;
+  b = 6;
+  c = this.a + this.b;
+  stateButton = false;
+  countClick = 0;
+  nombre:string = "";
+  apellido:string = "";
+
+  lat=19.4153569;
+  lng=-99.1360143;
+
+  items =[
+    {plan:'pagado',cercania:1,disrancia:1,age:30,status:true,name:'Miguel Mendoza'},
+    {plan:'gratuito',cercania:1,disrancia:1.8,age:17,status:true,name:'Beatriz Hernandez'},
+    {plan:'pagado',cercania:2,disrancia:4,age:33,status:true,name:'Leonard Cuenca'},
+    {plan:'gratuito',cercania:3,disrancia:5,age:15,status:true,name:'Dailin Morillo'},
+    {plan:'gratuito',cercania:3,disrancia:10,age:28,status:true,name:'Ricardo Diez'},
+    {plan:'pagado',cercania:3,disrancia:15,age:22,status:true,name:'Xenia'},
+    {plan:'gratuito',cercania:3,disrancia:20,age:39,status:false,name:'Gabriel Nicolas'},
+  ]
+
+  constructor (){
+    setTimeout(()=>{
+      this.stateButton = true
+    },3000);
+  }
+
+   doSomething(){
+      this.countClick++;
+      alert("Este es mi click: "+this.countClick);
+    }
+
+
+  //arrayTest:['1':['Hola']]
+
+
 }
--- cursos/angular4/platziSquare/src/app/app.module.ts
+++ cursos/angular4/platziSquare/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective,
+    ContarClicksDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB__18PtYBXeCmFlz6uCi1aiGj46GyNxW0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
